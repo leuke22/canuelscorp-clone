0413d3e7e07986e444bbf601c59c6758929029be
leuke <kenyamigable5@gmail.com>
2025-04-15 11:04:26 +0800

    complete auth functionality

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -8,6 +9,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
