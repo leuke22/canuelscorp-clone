6eda7c22e3fe840121f74f525f89865e9346f5ad
ken.amigable.22 <kenyamigable5@gmail.com>
2025-04-25 13:25:12 +0800

    add otp

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,8 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({ limit: "5mb" })); // to parse req.body
-// limit shouldn't be too high to prevent DOS
-app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
