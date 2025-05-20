1abe83d1bf7b0e8e2913bb0545d119c582ff5ffe
ken.amigable.22 <kenyamigable5@gmail.com>
2025-05-20 15:54:46 +0800

    Fixing problem in .env

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const axiosInstance = axios.create({
-  baseURL: "https://canuelscorp.onrender.com/api",
+  baseURL:
+    import.meta.env.DEV === "development"
+      ? "http://localhost:5000/api"
+      : import.meta.env.VITE_SERVER_URL + "/api",
   withCredentials: true,
 });
 
