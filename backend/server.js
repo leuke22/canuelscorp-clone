8c65383051c1c2bff84e44e37af52454e2f90056
ken.amigable.22 <kenyamigable5@gmail.com>
2025-05-20 14:01:47 +0800

    deployment for frontend

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,12 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
